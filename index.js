c4a6c8638fd61328660d3822af94e0375a3aa1ba
BeaSCarvalho <beaproscarva@gmail.com>
2022-02-05 19:36:10 -0300

    Updates

--- index.js
+++ index.js
@@ -1,62 +1,50 @@
 import validator from './validator.js';
 
 function getCardNumber(event) {
-
     event.preventDefault();
+
     let creditCardNumber = document.getElementById("number").value; 
-  
     let verifyQuant = verify(creditCardNumber);
-    if(verifyQuant == false){
+
+    if(!verifyQuant){
       alert("Quantidade de dígitos inválida(mínimo 13 e máximo 16)");
-    }
-  
-    else{
+      return;
+    } 
+
      let maskifyTheNumbers = validator.maskify(creditCardNumber);
-  
      let isValid = validator.isValid(creditCardNumber);
+     let resultMessage = "O cartão " + maskifyTheNumbers;
       if(isValid){
-       document.getElementById("result").innerHTML = "O cartão " + maskifyTheNumbers + " é válido para realizar compras online."; 
-      } 
-      else{
-      document.getElementById("result").innerHTML = "O cartão " + maskifyTheNumbers + " não é válido para realizar compras online. Entre em contato com a instituição financeira emissora do cartão para mais informações.";
+        resultMessage = `${resultMessage} " é válido para realizar compras online`; 
+      } else{
+        resultMessage = `${resultMessage} " não é válido para realizar compras online.
+         Entre em contato com a instituição financeira emissora do cartão para mais informações.`
       }
-    }  
+      document.getElementById("result").innerHTML = resultMessage;
+     
     document.getElementById("number").value='';
-  
-    return creditCardNumber;
   }
   
   document.getElementById("verify").addEventListener("click",getCardNumber);
   
   function verify(creditCardNumber) {                                            //função para ter entre 13 e 16 dígitos
-  
     let quantNumbers = creditCardNumber.length;                   
-    if(quantNumbers < 13) {
-      return false;
-    }
-    else if(quantNumbers > 16) {
-      return false;
-    }  
+    return quantNumbers >= 13 && quantNumbers <= 16;
   }
   
   function showButtonReset() {
-  
     let display = document.getElementById("reset").style.visibility;
   
     if (display == "none") {
      document.getElementById("reset").style.visibility = "hidden";
-    }
-    else {
+    } else{
      document.getElementById("reset").style.visibility = "visible";
     }
   
     document.getElementById("reset").style.visibility = "visible";
-  
-    return showButtonReset;
   }
   
   document.getElementById("verify").addEventListener("click",showButtonReset);
-  
   
   function tryAnotherNumber() {
     let creditCardNumber = document.getElementById("number"); 
@@ -64,8 +52,6 @@ function getCardNumber(event) {
     creditCardNumber.value = "";
     document.getElementById("result").innerHTML = "";
     document.getElementById("reset").style.visibility = "hidden";
-  
-    return tryAnotherNumber;
   } 
   
   document.getElementById("reset").addEventListener("click",tryAnotherNumber);
